08efe2149241ca9f14b339e59a015f4c34620bd2
anicoletupas <37381983+anicoletupas@users.noreply.github.com>
2019-04-24 21:43:58 -0700

    Add files via upload

--- upload.js
+++ upload.js
@@ -1,13 +1,35 @@
+JSONTest = function() {
 
-  var uploadedFile = document.getElementById('fileupload');
-  var Fs = require('fs')
-  var file = '/tmp/40mbfile'
-  var fileStream = Fs.createReadStream(file)
-  var fileStat = Fs.stat(file, function(err, stats) {
-  if (err) {
-    return console.log(err)
-  }
-  minioClient.putObject('images', '40mbfile', fileStream, stats.size, function(err, etag) {
-    return console.log(err, etag) // err should be null
-  })
+    var resultDiv = $("#resultDivContainer");
+
+    $.ajax({
+        url: "https://localhost:3000/images",
+        type: "POST",
+        dataType: "json",
+        success: function (result) {
+            switch (result) {
+                case true:
+                    processResponse(result);
+                    break;
+                default:
+                    resultDiv.html(result);
+            }
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+        alert(xhr.status);
+        alert(thrownError);
+        }
+    });
+};
+
+var Fs = require('fs')
+var file = '/tmp/40mbfile'
+var fileStream = Fs.createReadStream(file)
+var fileStat = Fs.stat(file, function(err, stats) {
+if (err) {
+  return console.log(err)
+}
+minioClient.putObject('images', '40mbfile', fileStream, stats.size, function(err, etag) {
+  return console.log(err, etag) // err should be null
+})
 })
