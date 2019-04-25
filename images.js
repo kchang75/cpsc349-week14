9e6d8851fcaf43eee6b6befdb8b67b38bdecf5df
Axelm3sh <axel.m3sh@gmail.com>
2019-04-24 20:24:05 -0700

    editing

---
+++ images.js
@@ -0,0 +1,40 @@
+$docBody = $(document.body);
+
+
+$docBody.ready(function(){
+    console.log("Loaded page");
+    
+    getImages(function (results){
+        console.log(JSON.parse(results));
+        
+        var obj = JSON.parse(results);
+        
+        obj.images.forEach(insertImg(item, index));
+        
+    })
+    
+});
+
+function getImages(callback) {
+    console.log("Requesting Posts");
+
+    var params = {
+        url: "http://localhost:3000/images",
+        method: "POST",
+        //        data: params,
+//        dataType: "json"
+    };
+
+    $.ajax(params).done(function (results) {
+        if (callback) {
+            //console.log(results);
+            callback.call(this, results);
+        }
+    });
+
+}
+
+function insertImg(item, index)
+{
+    
+}
