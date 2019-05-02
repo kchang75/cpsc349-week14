04b67cbc1c093b4a516099ea994126a13ee73c19
Axelm3sh <axel.m3sh@gmail.com>
2019-05-01 20:02:45 -0700

    testing new aws

--- upload.js
+++ upload.js
@@ -1,4 +1,22 @@
-JSONTest = function() {
+let s3Client = new AWS.S3({
+    endpoint: "http://127.0.0.1:9000",
+    s3ForcePAthStyle: true,
+    signatureVersion: "v4",
+    accessKeyId: "KWTCUUF8G2PMKTYITSDN",
+    secretAccessKey: "eiCN5aFmDO9giYIqjpdu8+BOMPA17z2J74bSHBmL"
+});
+
+let objParam = {
+    Bucket: "images",
+    Key: file.name,
+    Body: file,
+    ContentLength: file.size,
+    ContentType: file.type
+};
+
+s3Client.putObject(objParam, function (err, data) {console.log(err, data); });
+
+JSONTest = function () {
 
     var resultDiv = $("#resultDivContainer");
 
@@ -16,20 +34,20 @@ JSONTest = function() {
             }
         },
         error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
+            alert(xhr.status);
+            alert(thrownError);
         }
     });
 };
 
-var Fs = require('fs')
-var file = '/tmp/40mbfile'
-var fileStream = Fs.createReadStream(file)
-var fileStat = Fs.stat(file, function(err, stats) {
-if (err) {
-  return console.log(err)
-}
-minioClient.putObject('images', '40mbfile', fileStream, stats.size, function(err, etag) {
-  return console.log(err, etag) // err should be null
-})
-})
+//var Fs = require('fs')
+//var file = '/tmp/40mbfile'
+//var fileStream = Fs.createReadStream(file)
+//var fileStat = Fs.stat(file, function (err, stats) {
+//            if (err) {
+//                return console.log(err)
+//            }
+            //minioClient.putObject('images', '40mbfile', fileStream, stats.size, function(err, etag) {
+            //  return console.log(err, etag) // err should be null
+            //})
+            //})
